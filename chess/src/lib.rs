0bb80d09a0952318b889964f25fc3702bf43bbe6
David Trigell <david@Davids-MacBook-Pro.local>
2026-09-07 17:08:38 +0200

    Added move logic and function for demon piece. Improved readability of lib.rs. Started on README.md

--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -1,5 +1,13 @@
+
+//     ################################
+//     #                              #
+//     #         Structs/Enum         #
+//     #                              #
+//     ################################
+
+
 // Specifing what colors exist.
-// derive -> compiler can add prewritten implements.
+    // derive -> compiler can add prewritten implements.
 #[derive(Copy, Clone, Debug)]
 pub enum Color {
     White,
@@ -28,40 +36,80 @@ pub struct Piece {
 pub struct Board {
     pub squares: [Piece; 64],
 }
+
+
+//     ################################
+//     #                              #
+//     #      Static Variables        #
+//     #                              #
+//     ################################
+
+
+// Creates a null object = Empty, Empty
+static NULLTOKEN: Piece = Piece { 
+    color: Color::Empty, 
+    rank: Rank::Empty,
+};
+
+
+//     ################################
+//     #                              #
+//     #           Methods            #
+//     #                              #
+//     ################################
+
+
+// All chess logic.
 impl Board {
     // Creates a board object with nothing on it.
     pub fn init_board() -> Board {
-        // Creates a null object = Null,Null
-        let nulltoken = Piece { 
-            color: Color::Empty, 
-            rank: Rank::Empty,
-        };
         // Creates the board with demon on one square and rest null.
         let gameboard = Board { 
-            squares: [nulltoken; 64],
+            squares: [NULLTOKEN; 64],
         };
         return gameboard;
     }
-}
-// Places a demon onto the board.
-impl Board {
-    pub fn set_demon(&mut self, s: u32) {
+
+    // Places a demon onto the board.
+    pub fn set_demon(&mut self, start: usize) {
          // Creates a demon object = White,Demon
         let demon = Piece { 
             color: Color::White, 
             rank: Rank::Demon,
         }; 
         // Takes in a created Board object and sets the input square to the demon.      
-        self.squares[s as usize] = demon;
+        self.squares[start as usize] = demon;
     }
-}
 
-impl Board {
-    // Move a piece to any other square.
-    fn move_piece(&self, s: u32, n: u32) {
-        // match &self.squares[s] {
-            
-        // }
+    // Moves a piece from one square (start) to another (dest), IF ALLOWED.
+    pub fn move_piece(&mut self, start: usize, dest: usize) {
+        // Matches what rank is on the starting square to fetch logic.
+        match self.squares[start] {
+            // Matches for move demon
+            Piece {
+                color,
+                rank: Rank::Demon,
+            } => self.move_demon(start, dest),
+            // Placeholder
+            Piece {
+                color: Color,
+                rank: Rank,
+            } => return
+        }
     }
-}
 
+
+    //     ################################
+    //     #                              #
+    //     #         Move Logic           #
+    //     #                              #
+    //     ################################
+
+
+    // Move logic of the Demon rank.
+    fn move_demon(&mut self, start: usize, dest: usize) {
+        self.squares[dest as usize] = self.squares[start];
+        self.squares[start] = NULLTOKEN;
+    }
+
+}
--- libTest/src/main.rs
+++ libTest/src/main.rs
@@ -3,6 +3,21 @@ use chess::*;
 fn main() {
     let mut testboard = Board::init_board();
     testboard.set_demon(10); // Creates a demon on square 10.
-    let testsquare = testboard.squares[10]; // Need to implement indexmut method for Board.
-    println!("{:?}", testsquare)
+
+    let testsquare3 = testboard.squares[10]; // Checks what piece is on the square.
+    println!("Before move: {:?}", testsquare3);
+
+    testboard.move_piece(10, 15); // Moves the piece on square 10 to square 15.
+
+
+    let testsquare2 = testboard.squares[10]; // Checks what piece is on the square.
+    println!("original square: {:?}", testsquare2);
+
+    let testsquare1 = testboard.squares[15]; // Checks what piece is on the square.
+    println!("final square: {:?}", testsquare1)
+
+
+    
+
+    
 }
